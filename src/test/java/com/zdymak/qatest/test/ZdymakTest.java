90ce213f7b658257378ac2cd13d5c9ca9090c9fa
Eugene Kulik <yauhene.kulik@gmail.com>
2022-08-29 14:02:55 +0300

    test from idea with selenium

--- src/test/java/com/zdymak/qatest/test/ZdymakTest.java
+++ src/test/java/com/zdymak/qatest/test/ZdymakTest.java
@@ -2,15 +2,14 @@ package com.zdymak.qatest.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.zdymak.qatest.pages.ZdymakSingIn;
 import com.zdymak.qatest.pages.ZdymakMainPage;
+import com.zdymak.qatest.pages.ZdymakSingIn;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,6 +20,7 @@ public class ZdymakTest {
     @BeforeAll
     public static void setUpAll() {
         Configuration.browserSize = "1280x800";
+        Configuration.timeout = 40000;
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
@@ -37,7 +37,7 @@ public class ZdymakTest {
         $("#birthday").sendKeys("07-02-2008");
         $("#password").sendKeys("yGuS6QgZEN2biGDvT");
         $("#passwordCheck").sendKeys("yGuS6QgZEN2biGDvT");
-        $x("//button").click();
+        zdymakSingInPage.regButton.click();
         assertEquals(zdymakSingInPage.element3.getText(), $("div[class*=\"font-medium\"]").getText());
     }
 
